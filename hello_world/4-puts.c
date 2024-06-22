b813f6a59e38f94b2610a98d59da842742d28ab7
root <cavidagazad163@gmail.com>
2024-06-22 10:38:00 +0000

    puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-        char* str1 = "\"Programming is like building a multilingual puzzle";
-        puts(str1);
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
