27963e0486def31d51e51a98d6371546b5ad4032
root <cavidagaza163@gmail.com>
2024-06-23 07:29:13 +0000

    digits 0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
