dd4befa4c6e7b4ead7e5b8767059488a37502a60
root <cavidagaza163@gmail.com>
2024-06-23 07:36:46 +0000

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	for (char ch = 'a'; ch <= 'f'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
