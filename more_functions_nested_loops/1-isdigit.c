65f9bafe443765f4361290d847c83d8e45c962cc
root <cavidagazad163@gmail.com>
2024-06-28 08:03:06 +0000

    checking digits

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - Main entry
+ * @c: variable
+ * Return: checking digits, return 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
