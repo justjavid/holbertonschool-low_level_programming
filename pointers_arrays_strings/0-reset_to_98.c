80b624ea212f3ad91f7482cda434ca9b816e5566
root <cavidagazad163@gmail.com>
2024-06-29 12:52:24 +0000

    modify variable with pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Main entry
+ * @n: variable
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/** main - Entry point
+ *
+ * Description: 'the program's description'
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i=0; i<=7; i++)
+	_putchar(word[i]);
+	_putchar('\n');
+	return (0);
+}
