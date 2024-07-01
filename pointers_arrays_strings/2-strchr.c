ad8ff2a1a1b50efd3e30e7ff2b60d96522c3f69b
root <cavidagazad163@gmail.com>
2024-07-01 14:42:53 +0000

    string after given character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *  *_strchr - returns piece of string after the character.
+ *   *@s: variable
+ *    *@c: string
+ *     *Return: string after given character
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	while (*(s + count) != 0)
+	{
+		if (*(s + count) == c)
+		{
+			return (s + count);
+		}
+		count++;
+	}
+	return (NULL);
+}
