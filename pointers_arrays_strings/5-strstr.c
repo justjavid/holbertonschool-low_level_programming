8c81bedf0448561438a440e9db03a6dcae2c631d
root <cavidagazad163@gmail.com>
2024-07-01 16:29:09 +0000

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *  *_strpbrk - returns piece of string after the character.
+ *   *@s: pointer
+ *    *@accept: pointer
+ *     *Return: string after character
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int count1 = 0;
+	int count2 = 0;
+	int j;
+
+	while (*(needle + count1) != 0)
+	{
+		count1++;
+	}
+	while (*(haystack + count2) != 0)
+	{
+		j = 0;
+		while ((*(haystack + count2 + j) == *(needle + j)) && (j < count1))
+		{
+			if (j == count1 - 1)
+			return (haystack + count2);
+			j++;
+		}
+		count2++;
+	}
+	return (NULL);
+}
