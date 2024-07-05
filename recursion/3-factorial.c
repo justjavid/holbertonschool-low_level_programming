d522a0470d4360a89291608b6d9640936891fb12
root <cavidagazad163@gmail.com>
2024-07-05 08:25:53 +0000

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  *_strlen_recursion - returns factorial of given number.
+ *  *@n: given number
+ *  *Return: factorial of given number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	else if (n == 1 || n == 0)
+	return (1);
+	return (n * factorial(n - 1));
+}
