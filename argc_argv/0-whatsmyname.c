512354be7c83e2a52e21505b0747b4a2199b7484
root <cavidagazad163@gmail.com>
2024-07-07 14:32:14 +0000

    prints program's name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - Main Entry
+ *  @argc: variable that count things in command line.
+ *  @argv: array of pointers to those strings.
+ *  *Return: Success point 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
