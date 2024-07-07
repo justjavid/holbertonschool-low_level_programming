3a1e83468113f936b8d8aeaa4c09f69e5db34b56
root <cavidagazad163@gmail.com>
2024-07-07 16:18:21 +0000

    add only positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *  main - Main Entry
+ *  @argc: variable that count things in command line.
+ *  @argv: array of pointers to those strings.
+ *  *Return: Success point 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum, j;
+	
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) < 0)
+		continue;
+		j = 0;
+		while (argv[i][j] != 0)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+			j++;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
