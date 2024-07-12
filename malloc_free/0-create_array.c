3a3389a8aba5904a19403dc0a12a3957f39b4a68
cavid <cavidagazad163@gmail.com>
2024-07-12 14:17:49 +0400

    creating array with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 /**
- *  create_array - returns a pointer to array that we created. 
+ *  create_array - returns a pointer to array that we created.
  *  @c: variable
  *  @size: array  size
  *  *Return: pointer to array
@@ -9,17 +9,18 @@
 #include <stdlib.h>
 char *create_array(unsigned int size, char c)
 {
-        char *t;
-        unsigned int i = 0;
-       	t = malloc(sizeof(char) * size);
-        if (t == NULL || size == 0)
-        {
-                return (NULL);
-        }
-        while (i < size)
-        {
-                t[i] = c;
-                i++;
-        }
-        return (t);
+	char *t;
+	unsigned int i = 0;
+
+	t = malloc(sizeof(char) * size);
+	if (t == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		t[i] = c;
+		i++;
+	}
+	return (t);
 }
