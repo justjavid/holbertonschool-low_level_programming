30a182ded13d7dcb343f0b5d700ddcafbd43627c
cavid <cavidagazad163@gmail.com>
2024-07-12 16:48:44 +0400

    prints duplicated string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+/**
+ *  _strdup - returns a pointer to duplicated string.
+ *  @str: pointer
+ *  *Return: pointer to duplicated string
+ */
+#include "main.h"
+#include <stdlib.h>
+char *_strdup(char *str)
+{
+	char *t;
+	int len = 0;
+	int i = 0;
+
+	while (*(str + len) != 0)
+	{
+		len += 1;
+	}
+	t = malloc(sizeof(char) * len);
+	if (t == NULL || str  == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		t[i] = str[i];
+		i++;
+	}
+	return (t);
+}
