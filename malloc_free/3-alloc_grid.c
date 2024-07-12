e1e13e3bdde5d9f6b401cd67d2b375ae135a28b4
cavid <cavidagazad163@gmail.com>
2024-07-12 19:05:23 +0400

    pointer to 2D array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,25 +10,25 @@
 int **alloc_grid(int width, int height)
 {
 	int **t;
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	t = malloc(sizeof(int *) * height);
 	if (t == NULL)
 		return (NULL);
-	for (; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		t[i] = malloc(sizeof(int) * width);
 		if (!t[i])
 		{
-			for (; j <= i; j++)
-			free(t[j]);
+			for (j = 0; j <= i; j++)
+				free(t[j]);
 			free(t);
 		}
 		for (j = 0; j < width; j++)
-		t[i][j] = 0;
+			t[i][j] = 0;
 	}
 	return (t);
 }
