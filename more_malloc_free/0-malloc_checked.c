a303d532da2d680f3c7645b2b570786a6e75972f
cavid <cavidagazad163@gmail.com>
2024-07-12 21:01:53 +0400

    malloc checked with exit function

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+/**
+ *malloc_checked - function
+ * @b: variable
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+	return (t);
+}
