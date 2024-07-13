1d0f9ce40c2c431cc775f7ab63816e3fc0c579b0
cavid <cavidagazad163@gmail.com>
2024-07-13 21:14:17 +0400

    .

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  print_dog - print variable's elements given format
+ *  @d: pointer to struct dog variable
+ *  Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("\n");
+		exit(1);
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %2.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
