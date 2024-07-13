227e1a7e9c60d9620b16c94f5f53f0e66622ff86
cavid <cavidagazad163@gmail.com>
2024-07-13 21:27:09 +0400

    prints struct dog in given format

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,13 @@ void print_dog(struct dog *d)
 		printf("\n");
 		exit(1);
 	}
-	printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
