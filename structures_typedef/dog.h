ca3173e8da4a6b92de3bc8010f46fafcfc21b8c7
cavid <cavidagazad163@gmail.com>
2024-07-13 20:21:58 +0400

    dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  struct dog - dog structure
+ *  @name: name of dog 
+ *  @age: age of dog
+ *  @owner: owner of dog
+ *  
+ *  Description: dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
