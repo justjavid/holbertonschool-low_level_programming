9d9b38d7111589420713dbae81c6eb03ab4b62a3
cavid <cavidagazad163@gmail.com>
2024-07-15 19:56:27 +0400

    compare values for each element of array with function pointers

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ *  int_index - the index of the first element for
+ *  which the cmp function does not return 0.
+ *  @array: given array
+ *  @size: size of array
+ *  @cmp: function that compare values
+ *  Return: first index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
