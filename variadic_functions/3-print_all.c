8b0a41a306d704e9f7db912ebb472fcca7281328
cavid <cavidagazad163@gmail.com>
2024-07-17 19:19:40 +0400

    print anything with variadic function using switch case

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,29 +1,53 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 /**
  * print_all - prints anything that given.
- * @n: count of numbers
+ * @format: list of types of arguments passed into.
  * Return: Nothing
  */
 void print_all(const char * const format, ...)
 {
 	va_list arr;
-	int i = 0;
-	int j;
-	char args[] = "cifs";
+	unsigned long int i = 0;
+	char *str;
 
 	va_start(arr, format);
-
 	while (format[i] != '\0')
 	{
-		j = 0;
-		while(args[j] != '\0' && format[i] != args[j])
+		switch (format[i])
 		{
-			j++;
+			case 'c':
+				printf("%c", va_arg(arr, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(arr, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arr, double));
+				break;
+			case 's':
+				str = va_arg(arr, char*);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		if (i != strlen(format) - 1)
+		{
+			printf(", ");
+			i++;
+			continue;
+		}
+		i++;
 	}
-	va_end(arr);
-	printf("\n");
+	va_end(arr), printf("\n");
 }
