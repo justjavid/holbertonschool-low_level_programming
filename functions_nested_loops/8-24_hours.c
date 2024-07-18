1e54c0cb2a34c8bce3961018df35bbce91f9d033
cavid <cavidagazad163@gmail.com>
2024-07-18 22:01:19 +0400

    prints every minute of the day (24)

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *  jack_bauer - prints every minute of the day.
+ *  Return: nothing
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j;
+	int n;
+	int m;
+
+	while (i < 3)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (i == 2 && j == 4)
+				break;
+			n = 0;
+			while (n < 6)
+			{
+				m = 0;
+				while (m < 10)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + n);
+					_putchar('0' + m);
+					_putchar('\n');
+					m++;
+				}
+				n++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
