c864aeb32cc120885487bd4724734b0b8115f430
cavid <cavidagazad163@gmail.com>
2024-08-01 18:48:32 +0400

    binary representation of given number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  print_binary - prints binary representation of n.
+ *  @n: given number
+ *  Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int prod = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (n >= prod)
+		prod *= 2;
+	prod = prod >> 1;
+	while (prod)
+	{
+		if (n >= prod)
+		{
+			_putchar('1');
+			n = n - prod;
+		}
+		else 
+			_putchar('0');
+		prod = prod >> 1;
+	}
+}
