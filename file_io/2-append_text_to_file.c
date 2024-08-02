0a194a166c165faafd514c7ad026496174e53460
cavid <cavidagazad163@gmail.com>
2024-08-02 22:04:44 +0400

    appending text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
@@ -24,11 +24,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	while (text_content[i])
 		i++;
-	BWrite = write(fd, text_content, i - 1);
-	if (BWrite == i - 1)
+	BWrite = write(fd, text_content, i);
+	if (BWrite == -1)
 	{
-		close(fd);
-		return (1);
+		return (-1);
 	}
-	return (-1);
+	close(fd);
+	return (1);
 }
