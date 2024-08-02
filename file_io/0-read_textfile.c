bc81d606e9af0e5699fd851c4a44bfd27575d069
cavid <cavidagazad163@gmail.com>
2024-08-02 20:53:55 +0400

    reads a text file and prints it to standard output.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,7 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR || O_RDONLY, S_IROTH);
+	fd = open(filename, O_RDWR | O_RDONLY, S_IROTH | S_IWOTH);
 	if (fd == -1)
 		return (0);
 	BRead = read(fd, buffer, letters);
