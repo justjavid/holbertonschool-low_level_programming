2195a8ad1bee55e0ac4cf9fc260ad21678bafd59
cavid <cavidagazad163@gmail.com>
2024-08-02 21:02:05 +0400

    reads a text file and prints it to standard output.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,20 +3,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[2100];
+	char *buffer;
 	ssize_t BRead, BWrite;
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR | O_RDONLY, S_IROTH | S_IWOTH);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	BRead = read(fd, buffer, letters);
-	if (BRead == -1)
-		return (0);
-	BWrite = write(STDOUT_FILENO, buffer, (long unsigned int) BRead);
-	if (BWrite == -1)
-		return (0);
 	close(fd);
+	if (BRead == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	BWrite = write(STDOUT_FILENO, buffer, BRead);
+	free(buffer);
+	if (BWrite == -1 || BWrite != BRead)
+		return (0);
 	return (BWrite);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -2,5 +2,6 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
