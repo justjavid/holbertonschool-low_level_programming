ceb274960e48adf23cce7c15ccde7a877ca35fd7
cavid <cavidagazad163@gmail.com>
2024-08-02 21:41:44 +0400

    creating file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IREAD | S_IWRITE);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IREAD | S_IWRITE);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
