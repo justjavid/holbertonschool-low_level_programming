720cb81be620c7d2f7f68e57a1a1a07f7c11eb7b
cavid <cavidagazad163@gmail.com>
2024-08-02 21:36:59 +0400

    creating file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *  create_file - creates a file
+ *  @filename: name of file
+ *  @text_content: string to write the file
+ *  Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	ssize_t BWrite;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[i])
+		i++;
+	BWrite = write(fd, text_content, i);
+	if (BWrite == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
